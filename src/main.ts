b47adac51af01ad6a9d087c58ef64bd575f0c2ff
rgoldfinger-quizlet <roger.goldfinger@quizlet.com>
2020-07-10 09:30:43 -0700

    diff

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,111 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import { exec, ExecException } from 'child_process';
+import * as github from '@actions/github';
+import * as fs from 'fs';
+import * as path from 'path';
+import nodeFetch from 'node-fetch';
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+interface ExecResult {
+  err?: Error | undefined;
+  stdout: string;
+  stderr: string;
 }
 
-run()
+interface App {
+  metadata: { name: string };
+  spec: { source: { repoURL: string; path: string } };
+}
+const ARCH = process.env.ARCH || 'linux';
+const githubToken = core.getInput('GITHUB_TOKEN');
+const ARGOCD_SERVER_URL = core.getInput('ARGOCD_SERVER_URL');
+const ARGOCD_TOKEN = core.getInput('ARGOCD_TOKEN');
+const VERSION = core.getInput('VERSION');
+
+const octokit = github.getOctokit(githubToken);
+
+async function execCommand(command: string, failingExitCode = 1): Promise<ExecResult> {
+  const p = new Promise<ExecResult>(async (done, failed) => {
+    exec(command, (err: ExecException | null, stdout: string, stderr: string): void => {
+      const res: ExecResult = {
+        stdout,
+        stderr
+      };
+      if (err && err.code === failingExitCode) {
+        res.err = err;
+        failed(res);
+        return;
+      }
+      done(res);
+    });
+  });
+  return await p;
+}
+
+async function setupArgoCDCommand(): Promise<(params: string) => Promise<ExecResult>> {
+  const argoBinaryPath = 'bin/argo';
+  await tc.downloadTool(
+    `https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-${ARCH}-amd64`,
+    argoBinaryPath
+  );
+  fs.chmodSync(path.join(argoBinaryPath), '755');
+
+  core.addPath(argoBinaryPath);
+
+  return async (params: string) =>
+    execCommand(`${argoBinaryPath} ${params} --grpc-web --auth-token=${ARGOCD_TOKEN}`, 2);
+}
+
+async function getApps(): Promise<App[]> {
+  const url = `https://${ARGOCD_SERVER_URL}/api/v1/applications?fields=items.metadata.name,items.spec.source.path,items.spec.source.repoURL`;
+  console.log(url);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let responseJson: any;
+  try {
+    const response = await nodeFetch(url, {
+      method: 'GET',
+      headers: { Cookie: `argocd.token=${ARGOCD_TOKEN}` }
+    });
+    responseJson = await response.json();
+  } catch (e) {
+    core.error(e);
+  }
+  return (responseJson.items as App[]).filter(app => {
+    // TODO filter apps to only ones where they point to paths that have changed in this repo
+    return (
+      app.spec.source.repoURL ===
+      `https://github.com/${github.context.repo.owner}/${github.context.repo.repo}`
+    );
+  });
+}
+async function postDiffComment(appName: string, res: ExecResult): Promise<void> {
+  const output = `            
+  ArgoCD Diff for ${appName}:
+\`\`\`diff${res}\`\`\``;
+
+  octokit.issues.createComment({
+    issue_number: github.context.issue.number,
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    body: output
+  });
+}
+
+async function run(): Promise<void> {
+  const argocd = await setupArgoCDCommand();
+  const apps = await getApps();
+  console.log(apps);
+
+  // eslint-disable-next-line github/array-foreach
+  apps.forEach(async app => {
+    try {
+      const command = `app diff ${app.metadata.name} --local=${app.spec.source.path}`;
+      const res = await argocd(command);
+      await postDiffComment(app.metadata.name, res);
+    } catch (e) {
+      core.info(e);
+    }
+  });
+}
+
+run();
