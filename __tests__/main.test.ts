a17db99c4144724ca73effc5976f3d6838122ad7
rgoldfinger-quizlet <roger.goldfinger@quizlet.com>
2020-07-10 09:30:43 -0700

    diff

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,21 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+import * as process from 'process';
+import * as cp from 'child_process';
+import * as path from 'path';
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  process.env['ARCH'] = 'darwin';
+  process.env['GITHUB_REPOSITORY'] = 'quizlet/cd-infra';
+  process.env['INPUT_GITHUB_TOKEN'] = '500';
+  process.env['INPUT_VERSION'] = 'v1.6.1';
+  process.env['INPUT_ARGOCD_SERVER_URL'] = 'argocd.qzlt.io';
+  const ip = path.join(__dirname, '..', 'lib', 'main.js');
   const options: cp.ExecSyncOptions = {
     env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
-})
+  };
+  console.log(cp.execSync(`node ${ip}`, options).toString());
+});
+
+afterAll(() => {
+  cp.execSync('rm -rf bin');
+});
